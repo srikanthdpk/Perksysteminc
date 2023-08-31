67746c58f6216a5a497102b5fc58857acd6f729a
srikanthdpk <dpksrikanth95@gmail.com>
2023-08-31 16:55:25 +0530

    Updates

---
+++ precache-manifest.6cc1ec15531d4e2a2a42e50e9cdc6e76.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b3c98031ff7a805ef86",
+    "url": "/Perksysteminc/static/css/main.464cdb2b.chunk.css"
+  },
+  {
+    "revision": "3b3c98031ff7a805ef86",
+    "url": "/Perksysteminc/static/js/main.3b3c9803.chunk.js"
+  },
+  {
+    "revision": "e2723de9d6e9385cbe1e",
+    "url": "/Perksysteminc/static/js/1.e2723de9.chunk.js"
+  },
+  {
+    "revision": "52ce50198cc9c6fda0d3",
+    "url": "/Perksysteminc/static/css/2.b6e711e0.chunk.css"
+  },
+  {
+    "revision": "52ce50198cc9c6fda0d3",
+    "url": "/Perksysteminc/static/js/2.52ce5019.chunk.js"
+  },
+  {
+    "revision": "f8ab857ae52358b3e2b6",
+    "url": "/Perksysteminc/static/js/runtime~main.f8ab857a.js"
+  },
+  {
+    "revision": "8ec53405dfa56fcf7db737ab0ff2302d",
+    "url": "/Perksysteminc/static/media/client1.8ec53405.jfif"
+  },
+  {
+    "revision": "6d428949e6f44f6418e4d6b9747bb7b1",
+    "url": "/Perksysteminc/static/media/computer.6d428949.jpg"
+  },
+  {
+    "revision": "febff13477f163fd451498f5021ec4c4",
+    "url": "/Perksysteminc/static/media/abt.febff134.jfif"
+  },
+  {
+    "revision": "b14312074ebe4dd23c32ddf92050e9ae",
+    "url": "/Perksysteminc/static/media/about-us-banner.b1431207.png"
+  },
+  {
+    "revision": "7510e55f9c5c37972324a65682c380a0",
+    "url": "/Perksysteminc/static/media/abt_perksysinc.7510e55f.jpg"
+  },
+  {
+    "revision": "19d24ed23faccf86829fc65512508c5c",
+    "url": "/Perksysteminc/static/media/con1.19d24ed2.jpg"
+  },
+  {
+    "revision": "f0b5a69c1474c3a1bcfb45e2413ba773",
+    "url": "/Perksysteminc/static/media/con2.f0b5a69c.png"
+  },
+  {
+    "revision": "36c95c148036706f2fbde8c6948968f5",
+    "url": "/Perksysteminc/static/media/servlogo.36c95c14.jpg"
+  },
+  {
+    "revision": "5eea08860d9544505b09573b4c69b6ba",
+    "url": "/Perksysteminc/static/media/serlogo1.5eea0886.jfif"
+  },
+  {
+    "revision": "86d46bae25c18bc1860565132d488f91",
+    "url": "/Perksysteminc/static/media/services.86d46bae.jpg"
+  },
+  {
+    "revision": "df822c0f326a31f5f8bfd168a3f94d83",
+    "url": "/Perksysteminc/static/media/client.df822c0f.jfif"
+  },
+  {
+    "revision": "d49c38cbb2b0695c1a2e4dacad89ea73",
+    "url": "/Perksysteminc/static/media/login.d49c38cb.jfif"
+  },
+  {
+    "revision": "a30fb81bd52143bcd4de2898422ac8b9",
+    "url": "/Perksysteminc/static/media/bootstrap-icons.a30fb81b.woff2"
+  },
+  {
+    "revision": "3d0ec57f40d62ba6a8e0d1857bb8f7d5",
+    "url": "/Perksysteminc/static/media/bootstrap-icons.3d0ec57f.woff"
+  },
+  {
+    "revision": "c01e00fca417b63df7070c8c781de9a1",
+    "url": "/Perksysteminc/index.html"
+  }
+];
